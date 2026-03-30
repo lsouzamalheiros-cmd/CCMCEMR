921746c80eb3e8a21b7c2c660a238a84bf61d548
lsouzamalheiros-cmd <lsouza.malheiros@gmail.com>
2026-03-30 16:05:20 -0300

    Update app.py

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ def agora_local():
 # --- Conexão com MongoDB ---
 @st.cache_resource
 def conectar():
-    uri = "mongodb+srv://lsouzamalheiros_db_user:malheiros76@cluster0.10ycbss.mongodb.net/"
+    uri = "mongodb+srv://lsouzamalheiros_db_user:malheiros76@cluster0.10ycbss.mongodb.net/escola?retryWrites=true&w=majority"
     cliente = MongoClient(uri)
     return cliente["escola"]
 
